33427039e69fb939254295ba284dc8c94fcc0e9d
9o8 <99731724+youngexecutioner@users.noreply.github.com>
2022-04-28 04:14:14 -0400

    Add files via upload

---
+++ Windows/install.bat
@@ -0,0 +1,7 @@
+echo "------------------------------------------\n- We Are Installing Required Packages\n------------------------------------------"
+pip install pyfiglet
+pip install termcolor
+echo "------------------------------------------\n- Making Starter File In This Folder\n------------------------------------------"
+echo Making File...
+echo "cls\npython ../loadme.py" > start.bat
+echo "------------------------------------------\n- Tool Was Successfully Installed\n------------------------------------------"
