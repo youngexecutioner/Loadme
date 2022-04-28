618ec052fd1f207dbdd8165287e9578da96bcb30
9o8 <99731724+youngexecutioner@users.noreply.github.com>
2022-04-28 04:15:05 -0400

    Add files via upload

---
+++ Unix (Linux&Mac)/install.sh
@@ -0,0 +1,7 @@
+echo "------------------------------------------\n- We Are Installing Required Packages\n------------------------------------------"
+pip install pyfiglet
+pip install termcolor
+echo "------------------------------------------\n- Making Starter File In This Folder\n------------------------------------------"
+echo Making File...
+echo "clear\npython3 ../loadme.py" > start.sh
+echo "------------------------------------------\n- Tool Was Successfully Installed\n------------------------------------------"
